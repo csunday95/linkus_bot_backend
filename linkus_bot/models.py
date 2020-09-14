16cd8f1c1a4b8724c715ce87d0375db8da4edab7
Christopher Sund <csunday95@gmail.com>
2020-09-14 15:59:49 -0700

    added moderator snowflake to event model

--- linkus_bot/models.py
+++ linkus_bot/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 DISCIPLINE_NAME_MAX_LENGTH = 128
-SNOWFLAKE_MAX_LENGTH = 128
 USERNAME_MAX_LENGTH = 256
 
 
@@ -26,9 +25,8 @@ class UserDisciplineEvent(models.Model):
     class Meta:
         verbose_name_plural = 'User Discipline Events'
 
-    discord_user_snowflake = models.CharField(
-        max_length=SNOWFLAKE_MAX_LENGTH,
-        verbose_name='Discord User Snowflake',
+    discord_user_snowflake = models.BigIntegerField(
+        verbose_name='Disciplined Discord User Snowflake',
         help_text='the Discord snowflake of the user discipline by this instance'
     )
     discipline_type = models.ForeignKey(
@@ -42,15 +40,36 @@ class UserDisciplineEvent(models.Model):
         verbose_name='Username When Disciplined',
         help_text='The username of the user at the time of the issuance of this discipline'
     )
+    moderator_user_snowflake = models.BigIntegerField(
+        verbose_name='Moderator Discord User Snowflake',
+        help_text='The Discord snowflake of the moderator performing the discipline'
+    )
     reason_for_discipline = models.TextField(
         verbose_name='Reason for Discipline',
         help_text='The reason for this discipline instance'
     )
     discipline_end_date_time = models.DateTimeField(
         verbose_name='Discipline End Date/Time',
-        help_text='The time at which this discipline will be removed automatically'
+        help_text='The time at which this discipline will be removed automatically; if empty, discipline is permanent',
+        null=True,
+        blank=True
     )
     is_pardoned = models.BooleanField(
         verbose_name='Is Pardoned',
         help_text='If checked, this user has been pardoned and this discipline is not in effect'
     )
+
+    def __str__(self):
+        s = '{} [{}] - {}'.format(
+            self.username_when_disciplined,
+            self.discord_user_snowflake,
+            self.discipline_type.discipline_name
+        )
+        if self.discipline_end_date_time is not None:
+            s += ' until {}'.format(self.discipline_end_date_time)
+        if self.is_pardoned:
+            s += ' (PARDONED)'
+        return s
+
+    def __repr__(self):
+        return str(self)
--- linkus_bot/views.py
+++ linkus_bot/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 from .serializers import *
 from .models import *
 
@@ -7,6 +10,17 @@ from .models import *
 class UserDisciplineTypeViewSet(viewsets.ModelViewSet):
     queryset = UserDisciplineType.objects.all()
     serializer_class = UserDisciplineTypeSerializer
+
+    @action(detail=False)
+    def get_by_name(self, request: Request):
+        params = request.query_params
+        name = params['name']
+        try:
+            disc_type = UserDisciplineType.objects.get(discipline_name=name)
+        except UserDisciplineType.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(disc_type)
+        return Response(serializer.data)
 
 
 class UserDisciplineEventViewSet(viewsets.ModelViewSet):
