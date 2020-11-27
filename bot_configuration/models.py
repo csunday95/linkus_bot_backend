5498af2e07422f07276533eee7bb7109cc54ea89
Christopher Sund <csunday95@gmail.com>
2020-11-26 22:28:16 -0800

    added some notes for configuration model

--- bot_configuration/models.py
+++ bot_configuration/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+"""
+ - moderation channel / moderation role
+ - logging channel
+ - 
+ - default description append format text on add/create
+ - if multiple mappings for one emote are allowed
+ - if multiple mappings for one role are allowed
+"""
+
 
 class BotGuildConfiguration(models.Model):
     guild_snowflake = models.BigIntegerField(
