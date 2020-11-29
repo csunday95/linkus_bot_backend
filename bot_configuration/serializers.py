7dbc8c6b348b6ce4395fa9c0ae7992ed2f85bb9f
Christopher Sund <csunday95@gmail.com>
2020-11-28 22:47:45 -0800

    implemented update function for configuration endpoint

--- bot_configuration/serializers.py
+++ bot_configuration/serializers.py
@@ -35,3 +35,43 @@ class BotGuildConfigurationSerializer(serializers.ModelSerializer):
                 ReactionSubConfiguration.objects.create(**reaction_data)
             created_guild_config = BotGuildConfiguration.objects.create(**validated_data)
         return created_guild_config
+
+    def update(self, instance: BotGuildConfiguration, validated_data):
+        """update the given instance with the new patched data"""
+        instance.logging_channel_snowflake = validated_data.get(
+            'logging_channel_snowflake', instance.logging_channel_snowflake
+        )
+        if 'discipline_configuration' in validated_data:
+            new_discipline_config = validated_data['discipline_configuration']
+            current_discipline_config = instance.discipline_configuration
+            current_discipline_config.moderation_channel_only = new_discipline_config.get(
+                'moderation_channel_only', current_discipline_config.moderation_channel_only
+            )
+            current_discipline_config.moderation_role_snowflake = new_discipline_config.get(
+                'moderation_role_snowflake', current_discipline_config.moderation_role_snowflake
+            )
+            current_discipline_config.moderation_channel_only = new_discipline_config.get(
+                'moderation_channel_only', current_discipline_config.moderation_channel_only
+            )
+            current_discipline_config.save()
+        if 'reaction_configuration' in validated_data:
+            new_react_config = validated_data['reaction_configuration']
+            current_react_config = instance.reaction_configuration
+            current_react_config.default_description_line = new_react_config.get(
+                'default_description_line', current_react_config.default_description_line
+            )
+            current_react_config.allow_unmapped_reactions = new_react_config.get(
+                'allow_unmapped_reactions', current_react_config.allow_unmapped_reactions
+            )
+            current_react_config.allow_multiple_emotes_per_role = new_react_config.get(
+                'allow_multiple_emotes_per_role', current_react_config.allow_multiple_emotes_per_role
+            )
+            current_react_config.allow_multiple_roles_per_emote = new_react_config.get(
+                'allow_multiple_roles_per_emote', current_react_config.allow_multiple_roles_per_emote
+            )
+            current_react_config.remove_roles_on_react_remove = new_react_config.get(
+                'remove_roles_on_react_remove', current_react_config.remove_roles_on_react_remove
+            )
+            current_react_config.save()
+        instance.save()
+        return instance
